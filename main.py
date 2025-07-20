271aad336fc661cc681125130132d79c8bf01e7c
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2025-07-19 22:25:45 -0300

    endpoint produtos adicionado

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
+from typing import Any, Dict, List
 from fastapi import FastAPI
 
 app = FastAPI()
 
+produtos: List[Dict[str, Any]] = [
+    {
+        "id": 1,
+        "titulo": "Cadeira Gamer",
+        "descricao": "Cadeira confortável para fazer live",
+        "preço": 5.0,
+    },
+    {
+        "id": 2,
+        "a titulo": "Workshop",
+        "descricao": "Workshop de deploy",
+        "preço": 100.00,
+    },
+    {
+        "id": 3,
+        "a titulo": "Iphone",
+        "descricao": "Iphone 14",
+        "preço": None,
+    },
+]
+
 @app.get("/")
 def ola_mundo():
     return {"message": "Olá, mundo!"}
+
+@app.get("/produtos")
+def listar_produtos():
+    return produtos
+
+
--- testes.py
+++ testes.py
@@ -12,3 +12,11 @@ def test_ola_mundo():
 def test_ola_mundo_conteudo():
     response = client.get("/")
     assert response.json() == {"message": "Olá, mundo!"}
+
+def test_listar_produtos_status_code():
+    response = client.get("/produtos")
+    assert response.status_code == 200
+
+def test_tamanho_da_lista_produtos():
+    response = client.get("/produtos")
+    assert len(response.json()) == 3
